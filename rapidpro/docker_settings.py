85cfd8f62246a9c4cf21ef28e41af629c974585a
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-11-08 15:12:53 +0100

    Conventional settings

---
+++ rapidpro/docker_settings.py
@@ -0,0 +1,58 @@
+# -----------------------------------------------------------------------------------
+# Sample RapidPro settings file, this should allow you to deploy RapidPro locally on
+# a PostgreSQL database.
+#
+# The following are requirements:
+#     - a postgreSQL database named 'temba', with a user name 'temba' and
+#       password 'temba' (with postgis extensions installed)
+#     - a redis instance listening on localhost
+# -----------------------------------------------------------------------------------
+
+import warnings
+import os
+import dj_database_url
+from .settings_common import *  # noqa
+
+DEBUG = True
+
+STORAGE_URL = "http://localhost:8000/media"
+
+# allow all hosts in dev
+ALLOWED_HOSTS = ["*"]
+
+INTERNAL_IPS = ("127.0.0.1",)
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=60,
+    )
+}
+
+# -----------------------------------------------------------------------------------
+# Mailroom - localhost for dev, no auth token
+# -----------------------------------------------------------------------------------
+MAILROOM_URL = os.environ.get("MAILROOM_URL", "http://localhost:8090")
+MAILROOM_AUTH_TOKEN = None
+
+# -----------------------------------------------------------------------------------
+# In development, add in extra logging for exceptions and the debug toolbar
+# -----------------------------------------------------------------------------------
+MIDDLEWARE = ("temba.middleware.ExceptionMiddleware",) + MIDDLEWARE
+
+# -----------------------------------------------------------------------------------
+# In development, perform background tasks in the web thread (synchronously)
+# -----------------------------------------------------------------------------------
+CELERY_TASK_ALWAYS_EAGER = False
+CELERY_TASK_EAGER_PROPAGATES = True
+
+# -----------------------------------------------------------------------------------
+# This setting throws an exception if a naive datetime is used anywhere. (they should
+# always contain a timezone)
+# -----------------------------------------------------------------------------------
+warnings.filterwarnings(
+    "error", r"DateTimeField .* received a naive datetime", RuntimeWarning, r"django\.db\.models\.fields"
+)
+
+# -----------------------------------------------------------------------------------
+# Make our sitestatic URL be our static URL on development
+# -----------------------------------------------------------------------------------
+STATIC_URL = "/sitestatic/"
