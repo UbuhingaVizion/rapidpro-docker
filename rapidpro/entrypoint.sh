e4bcc492b6febe63e575709a6b62269d02828e04
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2025-11-09 11:03:07 +0100

    Prod settings

--- rapidpro/entrypoint.sh
+++ rapidpro/entrypoint.sh
@@ -6,9 +6,10 @@ ACTION=${1:-webapp}
 
 if [ "$ACTION" = "webapp" ]; then
     echo "Running RapidPro webapp..."
-	poetry run python3 manage.py migrate
-    poetry run python3 manage.py runserver 0.0.0.0:8000
+    poetry run python3 manage.py migrate
+    poetry run python3 manage.py collectstatic --noinput --clear
+    exec poetry run gunicorn temba.wsgi:application --bind 0.0.0.0:8000 --workers 4
 elif [ "$ACTION" = "celery" ]; then
     echo "Running RapidPro celery worker..."
-	poetry run celery -A temba worker -E -B --loglevel=INFO
+    exec poetry run celery -A temba worker -E -B --loglevel=INFO
 fi
